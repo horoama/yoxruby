6f330629e67d1fdf7b84cf7c507dd14fcf90adde
horoama <horoama929@gmail.com>
2016-12-19 20:07:20 +0900

    add yo,yoall&unread function

--- lib/yoxruby.rb
+++ lib/yoxruby.rb
@@ -1,5 +1,41 @@
 require "yoxruby/version"
+require "httpclient"
+require "json"
 
 module Yoxruby
-  # Your code goes here...
+    class Client
+        API_BASE_URL = "http://api.justyo.co"
+        def initialize(api_token=nil, access_token=nil)
+            if api_token != nil
+                @api_token = api_token
+            end
+            if access_token !=nil
+                @access_token = access_token
+            end
+            @httpclient = HTTPClient.new
+        end
+
+        def yoall(**options)
+            just_yo("/yoall/", options)
+        end
+
+        def yo(**options)
+            just_yo("/yo/", options)
+        end
+
+        def unread
+            url = API_BASE_URL + "/yos/?access_token=#{@access_token}"
+            res = @httpclient.get(url)
+        end
+
+        private
+        def just_yo(endpoint, **options)
+            url = API_BASE_URL + endpoint
+            res = @httpclient.post(url, {username: options[:username],
+                                         api_token: @api_token,
+                                         link: options[:link],
+                                         location: options[:location],
+                                         text: options[:text]} )
+        end
+    end
 end
